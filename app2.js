de83beeae3c0cb6b93341e7344851c9a9f24fa77
prasadp <prasad.patil@biascorp.com>
2021-03-24 09:14:53 -0400

    Removed second app

--- app2.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send('Hello, I am the 2nd application!'))
-app.listen(3005, () => console.log('Server ready'))
